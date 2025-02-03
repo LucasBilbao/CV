91adc479c7277c5e75cc2923f018af339cd18769
LucasBilbao <lucasbm789@gmail.com>
2025-02-04 01:17:03 +0400

    feat: make nav links dynamic

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
+import { NAV_PATHS } from '../../../utils/constants';
 
 @Component({
   selector: 'cv-nav',
@@ -9,4 +10,6 @@ import { MatMenuModule } from '@angular/material/menu';
   styleUrl: './nav.component.scss',
   imports: [RouterModule, MatIcon, MatMenuModule],
 })
-export class NavComponent {}
+export class NavComponent {
+  public readonly NAV_PATHS = NAV_PATHS;
+}
